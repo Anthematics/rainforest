e660becd140dd7f7b8386ac4ca5fe49eb1348c69
tenzin2017 <tenzinleo@gmail.com>
2017-09-07 14:36:41 -0400

    V 0.5 Added Product Form - Next step : submit form controller edits

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,3 @@ validates :name, :description , :price_in_cents, presence: true
 validates :price_in_cents, numericality: true
 validates :price_in_cents, numericality: { only_integer: true }
 end
-""

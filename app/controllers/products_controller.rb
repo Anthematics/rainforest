a384bf7baf53b808f4fb1aeee9561051efc11620
tenzin2017 <tenzinleo@gmail.com>
2017-09-07 14:01:25 -0400

    V0.3 Added working product show pages next step: Validations

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+   @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to "/products"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to "/products/#{@product.id}"
+    else
+      render :edit
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+name: "Iphone7",
+description: "It's the newest Iphone YET!!",
+price_in_cents: 1000
+)
+
+Product.create(
+name: "Computer",
+description: "It's a working laptop",
+price_in_cents: 1200
+)
+
+Product.create(
+name: "Bluetooth Keyboard",
+description: "Bluetooth keyboard with working thingy",
+price_in_cents: 50
+)
+
+Product.create(
+name: "Bose Headphones",
+description: "Super-Rad Noise Cancelling Headphones",
+price_in_cents: 500
+)
+
+Product.create(
+name: "Dining Table",
+description: "You can eat off of it",
+price_in_cents: 600 
+)
+
+Product.create(
+name: "Office Chair",
+description: "its an office chair , it even has SETTINGS",
+price_in_cents: 100
+)
+
+Product.create(
+name: "Banana",
+description: "It's a organic banana harvested from only the most sustainable jungles of the amazon",
+price_in_cents: 10
+)

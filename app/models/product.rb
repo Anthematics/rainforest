1d3c30ec2107d2dbb38ace5dbb094d54023066a3
tenzin2017 <tenzinleo@gmail.com>
2017-09-07 15:08:52 -0400

    V0.6 Added Error messages fixed bug where same error displayed twice

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
 validates :name, :description , :price_in_cents, presence: true
-validates :price_in_cents, numericality: true
 validates :price_in_cents, numericality: { only_integer: true }
 end

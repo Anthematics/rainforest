9828cc70d9499cc5e6a15269979c06f067090114
Anthematics <jasonharder2@gmail.com>
2017-09-08 15:48:51 -0400

    V1.3 working form submission for reviews and displays properly on the show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,57 +1,58 @@
 class ProductsController < ApplicationController
 
-  def index
-    @products = Product.all
-  end
+	def index
+		@products = Product.all
+	end
 
-  def show
-   @product = Product.find(params[:id])
-  end
+	def show
+	 @product = Product.find(params[:id])
+	 @review = Review.new
+	end
 
 
-  def new
-    @product = Product.new
-  end
+	def new
+		@product = Product.new
+	end
 
-  def create
-    @product = Product.new
+	def create
+		@product = Product.new
 
-    @product.name = params[:product][:name]
-    @product.description = params[:product][:description]
-    @product.price_in_cents = params[:product][:price_in_cents]
+		@product.name = params[:product][:name]
+		@product.description = params[:product][:description]
+		@product.price_in_cents = params[:product][:price_in_cents]
 
-    if @product.save
-      flash[:notice] = "You have successfully added #{@product.name} "
-      redirect_to "/products"
-    else
-      render :new
-    end
-  end
+		if @product.save
+			flash[:notice] = "You have successfully added #{@product.name} "
+			redirect_to "/products"
+		else
+			render :new
+		end
+	end
 
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    flash[:notice] = "You have successfully deleted #{@product.name} "
-    redirect_to "/products"
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		flash[:notice] = "You have successfully deleted #{@product.name} "
+		redirect_to "/products"
 
-  end
+	end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+	def edit
+		@product = Product.find(params[:id])
+	end
 
-  def update
-    @product = Product.find(params[:id])
+	def update
+		@product = Product.find(params[:id])
 
-    @product.name = params[:product][:name]
-    @product.description = params[:product][:description]
-    @product.price_in_cents = params[:product][:price_in_cents]
+		@product.name = params[:product][:name]
+		@product.description = params[:product][:description]
+		@product.price_in_cents = params[:product][:price_in_cents]
 
-    if @product.save
-      flash[:notice] = "You have successfully changed #{@product.name} "
-      redirect_to "/products/#{@product.id}"
-    else
-      render :edit
-    end
-  end
+		if @product.save
+			flash[:notice] = "You have successfully changed #{@product.name} "
+			redirect_to "/products/#{@product.id}"
+		else
+			render :edit
+		end
+	end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,57 +1,55 @@
 class ReviewsController < ApplicationController
 
-  def index
-    @reviews = Review.all
-  end
+	def index
+		@reviews = Review.all
+	end
 
 
-  def new
-    @review = Review.new
-  end
+	def new
+		@review = Review.new
+	end
 
-  def show
-   @review = Review.find(params[:id])
-  end
+	def show
+	 @review = Review.find(params[:id])
+	end
 
-  def create
-    @review = Review.new
+	def create
+		@product =Product.find(params[:product_id])
+		@review = @product.reviews.new #associates the two common elements of the join table (the foreign key)
+		#  that way they can connect and the reviews can properly show on the product show page.
+		@review.comment = params[:review][:comment]
+		@review.rating = params[:review][:rating]
 
-    @review.comment = params[:review][:comment]
-    @review.rating = params[:review][:rating]
-    @review.product_id = params[:review][:product_id]
+		if @review.save
+			flash[:notice] = "You have successfully added a review "
+			redirect_to "/products/#{@product.id}"
+		else
+			render "/products/show"
+		end
+	end
+	def destroy
+		@review = Review.find(params[:id])
+		@review.destroy
+		flash[:notice] = "You have successfully deleted the review "
+		redirect_to "/reviews"
+	end
 
-    if @review.save
-      flash[:notice] = "You have successfully added #{@review.name} "
-      redirect_to "/reviews"
-    else
-      render :new
-    end
-  end
-  def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
-    flash[:notice] = "You have successfully deleted #{@review.name} "
-    redirect_to "/reviews"
+	def edit
+		@review = Review.find(params[:id])
+	end
 
-  end
+	def update
+		@review = Review.find(params[:id])
 
-  def edit
-    @review = Review.find(params[:id])
-  end
+		@review.comment = params[:review][:comment]
+		@review.rating= params[:review][:rating]
+		@review.product_id = params[:review][:product_id]
 
-  def update
-    @review = Review.find(params[:id])
-
-    @review.comment = params[:review][:comment]
-    @review.rating= params[:review][:rating]
-    @review.product_id = params[:review][:product_id]
-
-    if @review.save
-      flash[:notice] = "You have successfully changed #{@review.name} "
-      redirect_to "/reviews/#{@review.id}"
-    else
-      render :edit
-
-
-
+		if @review.save
+			flash[:notice] = "You have successfully changed #{@review.name} "
+			redirect_to "/reviews/#{@review.id}"
+		else
+			render :edit
+		end
+	end
 end
